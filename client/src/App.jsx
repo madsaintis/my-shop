0c3b0b82c6f47015aedab5773efd11d0ae7b930c
madsaintis <muhammadafifazhan@graduate.utm.my>
2023-10-07 13:21:21 +0800

    Initialize repo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,29 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import Button from 'bootstrap'
+import './App.scss'
+import Button from 'react-bootstrap/Button';
+import Header from './components/Header';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div> test </div>
-        
+
+      <Header />
+
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
     </>
-  )
+  );
 }
 
 export default App
