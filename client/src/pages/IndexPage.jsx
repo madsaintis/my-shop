66d371f9c1165fe17cf52349064cfafcf2f40622
madsaintis <muhammadafifazhan@graduate.utm.my>
2023-11-14 23:21:53 +0800

    Updated package.json

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,22 +1,7 @@
-
-import Button from 'react-bootstrap/Button';
-
 export default function IndexPage() {
   
   return (
     <div>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Button variant="primary">Primary</Button>
-    <Button variant="secondary">Secondary</Button>
-    <Button variant="success">Success</Button>
-    <Button variant="warning">Warning</Button>
-    <Button variant="danger">Danger</Button>
-    <Button variant="info">Info</Button>
-    <Button variant="light">Light</Button>
-    <Button variant="dark">Dark</Button>
-    <Button variant="link">Link</Button>
     </div>
   )
 }
